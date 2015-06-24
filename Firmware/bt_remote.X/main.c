91f631e02b6fb11a44c411b13168fd38e0e309df
Karri Kivela <karrister@gmail.com>
2015-06-25 00:21:59 +0300

    Pinout
    Added pinout comment, init stub

--- Firmware/bt_remote.X/main.c
+++ Firmware/bt_remote.X/main.c
@@ -8,10 +8,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
+
+void init(void) {
+
+    /* Set pinmuxes and other per-pin config */
+    //4 - icsp/low batt indicator (input, hi = good, lo = low batt)
+    //5 - nikon shutter focus (output, hi = focus camera)
+    //6 - nikon shutter release (output, hi = release shutter)
+    //7 - led2 (output, active hi)
+    //8 - reset (output, active hi?)
+    //9 - bt status (input)
+    //10 - uart tx
+    //11 - sw3 (input. switch open = hi, switch closed = lo)
+    //12 - uart rx
+    //13 - led1 (output, active hi)
+    //14 - mode sw (input. switch open = hi, switch closed = lo)
+    //15 - sw2 (input. switch open = hi, switch closed = lo)
+    //16 - sw1 (input. switch open = hi, switch closed = lo)
+
+
+}
+
+
 int main(void) {
+
+    init();
 
     return 0;
 }
--- Firmware/bt_remote.X/project_includes.h
+++ Firmware/bt_remote.X/project_includes.h
@@ -12,6 +12,29 @@
 extern "C" {
 #endif
 
+/* PIC Pinout:
+ * 1 - vdd
+ * 2 - xtal
+ * 3 - xtal
+ * 4 - icsp/low batt indicator (input, hi = good, lo = low batt)
+ * 5 - nikon shutter focus (output, hi = focus camera)
+ * 6 - nikon shutter release (output, hi = release shutter)
+ * 7 - led2 (output, active hi)
+ * 8 - reset (output, active hi?)
+ * 9 - bt status (input)
+ * 10 - uart tx
+ * 11 - sw3 (input. switch open = hi, switch closed = lo)
+ * 12 - uart rx
+ * 13 - led1 (output, active hi)
+ * 14 - mode sw (input. switch open = hi, switch closed = lo)
+ * 15 - sw2 (input. switch open = hi, switch closed = lo)
+ * 16 - sw1 (input. switch open = hi, switch closed = lo)
+ * 17 - nc
+ * 18 - icsp
+ * 19 - icsp
+ * 20 - gnd
+ */
+
 
 
 
